9908c96c5a03dd08c387575f84b856fd86758d8e
Denylson <denylson.feitoza@alunos.ufersa.edu.br>
2025-06-15 23:10:00 -0300

    feat(utils): adiciona novos utilitarios

---
+++ app/backend/src/utils/dateUtils.ts
@@ -0,0 +1,14 @@
+export {
+  formatToYMD,
+  formatToDMY,
+  extractYMD,
+  generateAvailableHours,
+  isDateInRange,
+  isValidReservationDate,
+  isValidReservationTime,
+  isTimeInRange,
+  isWithinTableAvailability,
+  formatDate,
+  formatTime,
+  isValidDate,
+} from "../../../shared/utils";
