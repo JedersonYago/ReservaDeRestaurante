93ae77fc1999ddc1e4aab73b4f21be7b32939a72
denylsonfreitas <denylson.feitoza@alunos.ufersa.edu.br>
2025-07-26 15:01:10 -0300

    refactor(validation): atualiza o esquema de validação do usuário para exigir um nome com no mínimo 2 caracteres e adiciona verificação de correspondência entre senha e confirmação de senha

--- app/shared/validation/schemas.ts
+++ app/shared/validation/schemas.ts
@@ -1,22 +1,27 @@
 import { z } from "zod";
 
 // Schema de Usuário
-export const userSchema = z.object({
-  name: z.string().min(1, "O nome é obrigatório"),
-  email: z.string().email("Email inválido"),
-  username: z
-    .string()
-    .min(3, "O nome de usuário deve ter no mínimo 3 caracteres")
-    .max(30, "O nome de usuário deve ter no máximo 30 caracteres")
-    .regex(
-      /^[a-zA-Z0-9_]+$/,
-      "O nome de usuário deve conter apenas letras, números e underscore"
-    ),
-  password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres"),
-  confirmPassword: z.string(),
-  role: z.enum(["client", "admin"]).default("client"),
-  adminCode: z.string().optional(),
-});
+export const userSchema = z
+  .object({
+    name: z.string().min(2, "O nome deve ter no mínimo 2 caracteres"),
+    email: z.string().email("Email inválido"),
+    username: z
+      .string()
+      .min(3, "O nome de usuário deve ter no mínimo 3 caracteres")
+      .max(30, "O nome de usuário deve ter no máximo 30 caracteres")
+      .regex(
+        /^[a-zA-Z0-9_]+$/,
+        "O nome de usuário deve conter apenas letras, números e underscore"
+      ),
+    password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres"),
+    confirmPassword: z.string(),
+    role: z.enum(["client", "admin"]).default("client"),
+    adminCode: z.string().optional(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
+    path: ["confirmPassword"],
+  });
 
 export type UserSchema = z.infer<typeof userSchema>;
 
